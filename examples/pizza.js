667156b1875da161ad396f148cfff68439cdce43
Simon Boudrias <admin@simonboudrias.com>
2013-05-17 23:03:41 -0400

    Add a first basic example

---
+++ examples/pizza.js
@@ -0,0 +1,57 @@
+/**
+ * Pizza delivery prompt example
+ * run example by writing `node pizza.js` in your console
+ */
+
+"use strict";
+var inquirer = require("../inquirer");
+
+var questions = [
+  {
+    type: "confirm",
+    name: "toBeDelivered",
+    message: "Hi, welcome to Node Pizza plaza. Is it for a delivery?"
+  },
+  {
+    type: "input",
+    name: "phone",
+    message: "What's your phone number?"
+  },
+  {
+    type: "list",
+    name: "size",
+    message: "What size do you need?",
+    choices: [ "large", "medium", "small" ]
+  },
+  {
+    type: "list",
+    name: "toppings",
+    message: "What about the toping?",
+    choices: [
+      {
+        name: "Peperonni and chesse",
+        value: "PeperonniChesse"
+      },
+      {
+        name: "All dressed",
+        value: "alldressed"
+      },
+      {
+        name: "Hawaïan",
+        value: "hawain"
+      }
+    ]
+  },
+  {
+    type: "rawlist",
+    name: "liquor",
+    message: "You also get a free 2L liquor! Which one?",
+    choices: [ "Pepsi", "7up", "Coke" ]
+  }
+];
+
+inquirer.prompt(questions, function(answers) {
+  console.log();
+  console.log("Order receipt:");
+  console.log(JSON.stringify(answers, null, "  "));
+});
