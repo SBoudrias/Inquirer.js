241b77ab6928ea13321ee7a128759ec618aef8e2
Simon Boudrias <admin@simonboudrias.com>
2013-06-10 23:14:51 -0400

    Add unit test for runAsync

--- test/specs/utils.js
+++ test/specs/utils.js
@@ -16,3 +16,45 @@ describe("normalizeChoices", function() {
     }]);
   });
 });
+
+describe("runAsync", function() {
+
+  it("should run synchronous functions", function( done ) {
+    var aFunc = function() {
+      return "pass1";
+    };
+    utils.runAsync(aFunc, function( val ) {
+      expect(val).to.equal("pass1");
+      done();
+    });
+  });
+
+  it("should run synchronous functions", function( done ) {
+    var aFunc = function() {
+      var returns = this.async();
+      returns("pass2");
+    };
+    utils.runAsync(aFunc, function( val ) {
+      expect(val).to.equal("pass2");
+      done();
+    });
+  });
+
+  it("should pass single argument", function( done ) {
+    var aFunc = function( val ) {
+      expect(val).to.equal("a");
+      done();
+    };
+    utils.runAsync(aFunc, function() {}, "a");
+  });
+
+  it("should apply multiple arguments", function( done ) {
+    var aFunc = function( val, val2 ) {
+      expect(val).to.equal("a");
+      expect(val2).to.equal("b");
+      done();
+    };
+    utils.runAsync(aFunc, function() {}, "a", "b");
+  });
+
+});
