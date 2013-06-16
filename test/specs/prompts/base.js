878c80e463cb8973c7a545df452a949c087a05d1
Simon Boudrias <admin@simonboudrias.com>
2013-06-15 22:27:04 -0400

    Don't suffix a line that may be already suffixed. Fix #21

---
+++ test/specs/prompts/base.js
@@ -0,0 +1,28 @@
+var expect = require("chai").expect;
+var sinon = require("sinon");
+var ReadlineStub = require("../../helpers/readline");
+
+var Base = require("../../../lib/prompts/base");
+
+// Prevent prompt from writing to screen
+// Confirm.prototype.write = function() { return this; };
+
+describe("`base` prompt (e.g. prompt helpers)", function() {
+
+  beforeEach(function() {
+    this.rl = new ReadlineStub();
+    this.base = new Base({
+      message: "foo bar",
+      name: "name"
+    }, this.rl );
+  });
+
+  it("`suffix` method should only add ':' if last char is a letter", function() {
+    expect(this.base.suffix("m:")).to.equal("m:");
+    expect(this.base.suffix("m?")).to.equal("m?");
+    expect(this.base.suffix("m")).to.equal("m: ");
+    expect(this.base.suffix("m ")).to.equal("m ");
+    expect(this.base.suffix()).to.equal(": ");
+  });
+
+});
