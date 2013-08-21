e96d984f1876ee3c98194ceba9e4f0a18f694444
Simon Boudrias <simon.boudrias@cossette.com>
2013-08-21 11:26:20 -0400

    Refactor choices normalization
    Update utils.normalizeChoice to create new choices Object and to consider
    separator objects.

---
+++ test/specs/objects/choice.js
@@ -0,0 +1,32 @@
+var expect = require("chai").expect;
+var sinon = require("sinon");
+var ReadlineStub = require("../../helpers/readline");
+
+var Choice = require("../../../lib/objects/choice");
+
+describe("Choice object", function() {
+  it("should normalize accept String as value", function() {
+    var choice = new Choice("foo");
+    expect( choice.name ).to.equal("foo");
+    expect( choice.value ).to.equal("foo");
+  });
+
+  it("should use value|name as default if default property is missing", function() {
+    var onlyName = new Choice({ name : "foo" });
+    var onlyVal = new Choice({ value : "bar" });
+
+    expect( onlyName.name ).to.equal("foo");
+    expect( onlyName.value ).to.equal("foo");
+    expect( onlyVal.name ).to.equal("bar");
+    expect( onlyVal.value ).to.equal("bar");
+  });
+
+  it("should keep extra keys", function() {
+    var choice = new Choice({ name: "foo", extra: "1" });
+
+    expect( choice.extra ).to.equal("1");
+    expect( choice.name ).to.equal("foo");
+    expect( choice.value ).to.equal("foo");
+  });
+
+});
--- test/specs/utils.js
+++ test/specs/utils.js
@@ -1,34 +1,25 @@
 var expect = require("chai").expect;
 var sinon = require("sinon");
+
 var utils = require("../../lib/utils/utils");
+var inquirer = require("../../lib/inquirer");
+var Choice = require("../../lib/objects/choice");
 
 describe("normalizeChoices", function() {
 
-  it("should normalize array containing strings", function() {
-    var initial = [ "foo", "bar" ];
-    var normalized = utils.normalizeChoices(initial);
-    expect(normalized).to.eql([{
-      name: "foo",
-      value: "foo"
-    }, {
-      name: "bar",
-      value: "bar"
-    }]);
+  it("should create Choice object from array member", function() {
+    var result = utils.normalizeChoices([ "bar", { name: "foo" } ]);
+    expect( result[0] ).to.be.instanceOf( Choice );
+    expect( result[1] ).to.be.instanceOf( Choice );
   });
 
-  it("should keep extra keys", function() {
-    var initial = [{ name: "foo", extra: "1" }, { name: "bar", key: "z" }];
-    var normalized = utils.normalizeChoices(initial);
-    expect(normalized).to.eql([{
-      name: "foo",
-      value: "foo",
-      extra: "1"
-    }, {
-      name: "bar",
-      value: "bar",
-      key: "z"
-    }]);
+  it("should not process Separator object", function() {
+    var sep = new inquirer.Separator();
+    var result = utils.normalizeChoices([ "Bar", sep ]);
+    expect( result[0].name ).to.equal("Bar");
+    expect( result[1] ).to.equal( sep );
   });
+
 });
 
 describe("runAsync", function() {
