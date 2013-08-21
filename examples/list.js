f3aad7d7c3a99e6b0885d54d43d8a3492d27d02f
Simon Boudrias <simon.boudrias@cossette.com>
2013-08-21 11:53:30 -0400

    Implement separators support on the list prompt

--- examples/list.js
+++ examples/list.js
@@ -5,12 +5,26 @@
 "use strict";
 var inquirer = require("../lib/inquirer");
 
-inquirer.prompt({
-  type: "list",
-  name: "size",
-  message: "What size do you need",
-  choices: [ "Jumbo", "Large", "Standard", "Medium", "Small", "Micro" ],
-  filter: function( val ) { return val.toLowerCase(); }
-}, function( answers ) {
-  console.log( JSON.stringify(answers, null, "  ") );
-});
+inquirer.prompt([
+  {
+    type: "list",
+    name: "theme",
+    message: "What do you want to do?",
+    choices: [
+      "Order a pizza",
+      "Make a reservation",
+      new inquirer.Separator(),
+      "Ask opening hours",
+      "Talk to the receptionnist"
+    ]
+  },
+  {
+    type: "list",
+    name: "size",
+    message: "What size do you need",
+    choices: [ "Jumbo", "Large", "Standard", "Medium", "Small", "Micro" ],
+    filter: function( val ) { return val.toLowerCase(); }
+  }
+], function( answers ) {
+    console.log( JSON.stringify(answers, null, "  ") );
+  });
--- test/specs/objects/separator.js
+++ test/specs/objects/separator.js
@@ -5,25 +5,30 @@ var ReadlineStub = require("../../helpers/readline");
 var Separator = require("../../../lib/objects/separator");
 var Inquirer = require("../../../lib/inquirer");
 
-describe("Separator object", function() {
+describe("Separator constructor", function() {
 
-  it("should have a default", function() {
+  it("should set a default", function() {
     var sep = new Separator();
     expect( sep.toString() ).to.equal("--------");
   });
 
-  it("should use user input as separator", function() {
+  it("should set user input as separator", function() {
     var sep = new Separator("foo bar");
     expect( sep.toString() ).to.equal("foo bar");
   });
 
-  it("should be transformed to string when appended to a string", function() {
+  it("instances should be stringified when appended to a string", function() {
     var sep = new Separator("foo bar");
     expect( sep + "" ).to.equal("foo bar");
   });
 
   it("should be exposed on Inquirer object", function() {
     expect( Inquirer.Separator ).to.equal( Separator );
+  });
+
+  it("should expose a helper function to check for separator", function() {
+    expect( Separator.exclude({}) ).to.be.true;
+    expect( Separator.exclude(new Separator()) ).to.be.false;
   });
 
 });
