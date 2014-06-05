066220845269ec73e2a4ddae1ea4718d3ba4d3bf
Danny Douglass <ddouglass@gmail.com>
2014-06-05 15:05:59 -0700

    added recursive prompt example
    updates per PR feedback

---
+++ examples/recursive.js
@@ -0,0 +1,35 @@
+/**
+ * Recursive prompt example
+ * Allows user to choose when to exit prompt
+ */
+
+"use strict";
+var inquirer = require("../lib/inquirer");
+var output = [];
+
+var questions = [
+  {
+    type: "input",
+    name: "tvShow",
+    message: "What's your favorite TV show?"
+  },
+  {
+    type: "confirm",
+    name: "askAgain",
+    message: "Want to enter another TV show favorite (just hit enter for YES)?",
+    default: true
+  }
+];
+
+function ask() {
+  inquirer.prompt( questions, function( answers ) {
+    output.push( answers.tvShow );
+    if ( answers.askAgain ) {
+      ask();
+    } else {
+      console.log( "Your favorite TV Shows:", output.join(", ") );
+    }
+  });
+}
+
+ask();
