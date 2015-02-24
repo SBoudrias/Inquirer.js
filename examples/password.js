cb264caac38cc22188f2238a308c0cfb35200910
Tim Giblin <timmyg13@gmail.com>
2015-02-23 23:00:22 -0500

    adding password prompt that doesnt display any characters; resolves #177

--- examples/password.js
+++ examples/password.js
@@ -10,6 +10,11 @@ inquirer.prompt([
     type: "password",
     message: "Enter your git password",
     name: "password"
+  },
+  {
+    type: "password-hidden",
+    message: "Enter your git password again",
+    name: "password"
   }
 ], function( answers ) {
   console.log( JSON.stringify(answers, null, "  ") );
