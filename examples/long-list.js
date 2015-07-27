01d5f3c507272774890d48474d85759dfc40db50
Simon Boudrias <admin@simonboudrias.com>
2015-07-27 00:10:26 -0700

    Bump dependencies and add natural line return in lists example

--- examples/long-list.js
+++ examples/long-list.js
@@ -9,6 +9,7 @@ var choices = Array.apply(0, new Array(26)).map(function(x,y) {
   return String.fromCharCode(y + 65);
 });
 choices.push("Multiline option \n  super cool feature");
+choices.push("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.");
 
 inquirer.prompt([
   {
