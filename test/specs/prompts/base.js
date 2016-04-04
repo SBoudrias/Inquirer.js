9a260c562ec6fdbf737f12df555c0ee3160164d6
Simon Boudrias <admin@simonboudrias.com>
2016-04-03 20:40:11 -0700

    Get rid of unused functions

--- test/specs/prompts/base.js
+++ test/specs/prompts/base.js
@@ -1,5 +1,4 @@
 var expect = require('chai').expect;
-var chalk = require('chalk');
 var ReadlineStub = require('../../helpers/readline');
 
 var Base = require('../../../lib/prompts/base');
@@ -11,20 +10,6 @@ describe('`base` prompt (e.g. prompt helpers)', function () {
       message: 'foo bar',
       name: 'name'
     }, this.rl);
-  });
-
-  it('`suffix` method should only add \':\' if last char is a letter', function () {
-    expect(this.base.suffix('m:')).to.equal('m: ');
-    expect(this.base.suffix('m?')).to.equal('m? ');
-    expect(this.base.suffix('my question?')).to.equal('my question? ');
-    expect(this.base.suffix(chalk.bold('m'))).to.equal('\u001b[1mm:\u001b[22m ');
-    expect(chalk.stripColor(this.base.suffix(chalk.bold('m?')))).to.equal('m? ');
-    expect(this.base.suffix('m')).to.equal('m: ');
-    expect(this.base.suffix('M')).to.equal('M: ');
-    expect(this.base.suffix('m ')).to.equal('m ');
-    expect(this.base.suffix('9')).to.equal('9: ');
-    expect(this.base.suffix('9~')).to.equal('9~ ');
-    expect(this.base.suffix()).to.equal(': ');
   });
 
   it('should not point by reference to the entry `question` object', function () {
