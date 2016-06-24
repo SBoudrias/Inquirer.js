93e0a09259df240ab62527257f2ed0da98ae7269
Kevin Gravier <kgravier@fusetg.com>
2016-06-24 12:48:20 -0400

    Changed editor example, new external-editor package, removed un-needed dependencies

--- examples/editor.js
+++ examples/editor.js
@@ -9,9 +9,13 @@ var questions = [
   {
     type: 'editor',
     name: 'bio',
-    message: 'Please write a short bio',
-    default: function () {
-      return 'I am a very HAPPY HAPPY person.\n\nAlso, sometimes I go a long walks.';
+    message: 'Please write a short bio of at least 3 lines.',
+    validate: function (text) {
+      if (text.split('\n').length < 3) {
+        return 'Must be at least 3 lines.';
+      }
+
+      return true;
     }
   }
 ];
