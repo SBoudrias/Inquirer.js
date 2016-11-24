ae73e86e2b0bd88e6588dc9e44ea25e43fb4fa6d
Ihor Halchevskyi <ihorskyi@users.noreply.github.com>
2016-11-23 22:39:35 -0500

    Default value for expand can be 'help' (#457)
    * Default value for expand can be 'help'
    
    * Make help the default option if no default key is provided
    
    * tiny change requested by SBoudrias
    
    * simplifying even more :)
    
    * using `choices.realLength`

--- test/specs/prompts/expand.js
+++ test/specs/prompts/expand.js
@@ -44,14 +44,6 @@ describe('`expand` prompt', function () {
     expect(mkPrompt).to.throw(/Reserved key error/);
   });
 
-  it('should take the first choice by default', function (done) {
-    this.expand.run().then(function (answer) {
-      expect(answer).to.equal('acab');
-      done();
-    });
-    this.rl.emit('line');
-  });
-
   it('should allow false as a value', function () {
     var promise = this.expand.run();
 
@@ -131,6 +123,14 @@ describe('`expand` prompt', function () {
 
     this.expand.run();
     expect(this.rl.output.__raw__).to.contain('(aBcdh)');
+  });
+
+  it('should display and capitalize the default choice H (Help) `key` if none provided', function () {
+    delete this.fixture.default;
+    this.expand = new Expand(this.fixture, this.rl);
+    this.expand.run();
+
+    expect(this.rl.output.__raw__).to.contain('(abcdH)');
   });
 
   it('should \'autocomplete\' the user input', function (done) {
