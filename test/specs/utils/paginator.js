2356da2153d6dc847da56684775371ce07ff35b7
Ihor Halchevskyi <igor.galchevsky@gmail.com>
2017-02-06 18:26:27 -0500

    If pageHeight is not defined, then automatically use the terminal window size

--- test/specs/utils/paginator.js
+++ test/specs/utils/paginator.js
@@ -1,4 +1,5 @@
 /* eslint no-multi-spaces: "off" */
+/* eslint space-in-parens: "off" */
 /* eslint brace-style: "off" */
 /* eslint max-statements-per-line: "off" */
 var _ = require('lodash');
@@ -7,48 +8,43 @@ var sinon = require('sinon');
 var Paginator = require('../../../lib/utils/paginator');
 
 describe('Paginator', function () {
-  describe('Simple', function () {
-    var paginator;
-
-    beforeEach(function () {
-      paginator = new Paginator();
-    });
-
-    it('should return default height (7)', function () { expect(paginator.getHeight(undefined, _.range(5))).equal(7);  });
-    it('should return specified height',   function () { expect(paginator.getHeight(20,        _.range(5))).equal(20); });
-  });
-
   describe('Auto height (Terminal mode)', function () {
     var paginator;
+    var height;
 
     beforeEach(function () {
       process.stdout.getWindowSize = sinon.stub().returns([80, 20]);
       paginator = new Paginator();
+      height = paginator.getHeight.bind(paginator);
     });
 
-    it('should return lines.count    if <   autoHeight-3',  function () { expect(paginator.getHeight('auto', _.range(16))).equal(16); });
-    it('should return lines.count -3 if === autoHeight-3',  function () { expect(paginator.getHeight('auto', _.range(17))).equal(17); });
-    it('should return lines.count -3 if === autoHeight-2',  function () { expect(paginator.getHeight('auto', _.range(18))).equal(17); });
-    it('should return lines.count -3 if === autoHeight-1',  function () { expect(paginator.getHeight('auto', _.range(19))).equal(17); });
-    it('should return lines.count -3 if === autoHeight',    function () { expect(paginator.getHeight('auto', _.range(20))).equal(17); });
-    it('should return lines.count -3 if >   autoHeight+1',  function () { expect(paginator.getHeight('auto', _.range(21))).equal(17); });
-    it('should return lines.count -3 if >   autoHeight+2',  function () { expect(paginator.getHeight('auto', _.range(22))).equal(17); });
+    it('should return specified height',                    function () { expect(height(_.range(20), 10)).equal(10); });
+    it('should return lines.count    if <   autoHeight-3',  function () { expect(height(_.range(16)    )).equal(16); });
+    it('should return lines.count -3 if === autoHeight-3',  function () { expect(height(_.range(17)    )).equal(17); });
+    it('should return lines.count -3 if === autoHeight-2',  function () { expect(height(_.range(18)    )).equal(17); });
+    it('should return lines.count -3 if === autoHeight-1',  function () { expect(height(_.range(19)    )).equal(17); });
+    it('should return lines.count -3 if === autoHeight',    function () { expect(height(_.range(20)    )).equal(17); });
+    it('should return lines.count -3 if >   autoHeight+1',  function () { expect(height(_.range(21)    )).equal(17); });
+    it('should return lines.count -3 if >   autoHeight+2',  function () { expect(height(_.range(22)    )).equal(17); });
   });
 
   describe('Auto height (non Terminal mode)', function () {
     var paginator;
+    var height;
 
     beforeEach(function () {
       paginator = new Paginator();
       process.stdout.getWindowSize = undefined;
+      height = paginator.getHeight.bind(paginator);
     });
 
-    it('should return lines.count    if <   autoHeight-3',  function () { expect(paginator.getHeight('auto', _.range(16))).equal(7); });
-    it('should return lines.count -3 if === autoHeight-3',  function () { expect(paginator.getHeight('auto', _.range(17))).equal(7); });
-    it('should return lines.count -3 if === autoHeight-2',  function () { expect(paginator.getHeight('auto', _.range(18))).equal(7); });
-    it('should return lines.count -3 if === autoHeight-1',  function () { expect(paginator.getHeight('auto', _.range(19))).equal(7); });
-    it('should return lines.count -3 if === autoHeight',    function () { expect(paginator.getHeight('auto', _.range(20))).equal(7); });
-    it('should return lines.count -3 if >   autoHeight+1',  function () { expect(paginator.getHeight('auto', _.range(21))).equal(7); });
-    it('should return lines.count -3 if >   autoHeight+2',  function () { expect(paginator.getHeight('auto', _.range(22))).equal(7); });
+    it('should return specified height',                    function () { expect(height(_.range(20), 10)).equal(10); });
+    it('should return default height if <   autoHeight-3',  function () { expect(height(_.range(16)    )).equal(7);  });
+    it('should return default height if === autoHeight-3',  function () { expect(height(_.range(17)    )).equal(7);  });
+    it('should return default height if === autoHeight-2',  function () { expect(height(_.range(18)    )).equal(7);  });
+    it('should return default height if === autoHeight-1',  function () { expect(height(_.range(19)    )).equal(7);  });
+    it('should return default height if === autoHeight',    function () { expect(height(_.range(20)    )).equal(7);  });
+    it('should return default height if >   autoHeight+1',  function () { expect(height(_.range(21)    )).equal(7);  });
+    it('should return default height if >   autoHeight+2',  function () { expect(height(_.range(22)    )).equal(7);  });
   });
 });
