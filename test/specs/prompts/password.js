9de81a8b702f1d0bb8792443bdb455e20a0875d6
James Wyatt Cready <wyatt.cready@lanetix.com>
2017-04-07 14:02:17 -0400

    Fix linting of tests

--- test/specs/prompts/password.js
+++ test/specs/prompts/password.js
@@ -6,6 +6,18 @@ var fixtures = require('../../helpers/fixtures');
 
 var Password = require('../../../lib/prompts/password');
 
+function testMasking(rl, mask) {
+  return function (answer) {
+    expect(answer).to.equal('Inquirer');
+    var expectOutput = expect(stripAnsi(rl.output.__raw__));
+    if (mask) {
+      expectOutput.to.contain(mask);
+    } else {
+      expectOutput.to.not.contain('********');
+    }
+  };
+}
+
 describe('`password` prompt', function () {
   beforeEach(function () {
     this.fixture = _.clone(fixtures.password);
@@ -14,48 +26,27 @@ describe('`password` prompt', function () {
 
   it('should use raw value from the user without masking', function () {
     var password = new Password(this.fixture, this.rl);
-    var rl = this.rl;
+    var promise = password.run().then(testMasking(this.rl, false));
 
-    var promise = password.run().then(function (answer) {
-      expect(answer).to.equal('Inquirer');
-      expect(stripAnsi(rl.output.__raw__)).to.not.contain('********');
-    });
-
-    var prompt = stripAnsi(this.rl.output.__raw__)
     this.rl.emit('line', 'Inquirer');
     return promise;
   });
 
   it('should mask the input with "*" if the `mask` option was provided by the user was `true`', function () {
-    this.fixture.mask = true
+    this.fixture.mask = true;
     var password = new Password(this.fixture, this.rl);
-    var rl = this.rl;
+    var promise = password.run().then(testMasking(this.rl, '********'));
 
-    var promise = password.run().then(function (answer) {
-      expect(answer).to.equal('Inquirer');
-      expect(stripAnsi(rl.output.__raw__)).to.contain('********');
-    });
-
-    var prompt = stripAnsi(this.rl.output.__raw__)
     this.rl.emit('line', 'Inquirer');
-
     return promise;
   });
 
   it('should mask the input if a `mask` string was provided by the user', function () {
-    this.fixture.mask = '#'
+    this.fixture.mask = '#';
     var password = new Password(this.fixture, this.rl);
-    var rl = this.rl;
-    var fixture = this.fixture;
+    var promise = password.run().then(testMasking(this.rl, '########'));
 
-    var promise = password.run().then(function (answer) {
-      expect(answer).to.equal('Inquirer');
-      expect(stripAnsi(rl.output.__raw__)).to.contain('########');
-    });
-
-    var prompt = stripAnsi(this.rl.output.__raw__)
     this.rl.emit('line', 'Inquirer');
-
     return promise;
   });
 });
