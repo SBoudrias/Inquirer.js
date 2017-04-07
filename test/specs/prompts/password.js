d7db156e8485924d582110a520756ca3c6bf221a
James Wyatt Cready <wyatt.cready@lanetix.com>
2017-04-07 13:49:18 -0400

    Add some tests

--- test/specs/prompts/password.js
+++ test/specs/prompts/password.js
@@ -1,3 +1,4 @@
+var stripAnsi = require('strip-ansi');
 var expect = require('chai').expect;
 var _ = require('lodash');
 var ReadlineStub = require('../../helpers/readline');
@@ -11,14 +12,50 @@ describe('`password` prompt', function () {
     this.rl = new ReadlineStub();
   });
 
-  it('should use raw value from the user', function (done) {
+  it('should use raw value from the user without masking', function () {
     var password = new Password(this.fixture, this.rl);
+    var rl = this.rl;
 
-    password.run().then(function (answer) {
+    var promise = password.run().then(function (answer) {
       expect(answer).to.equal('Inquirer');
-      done();
+      expect(stripAnsi(rl.output.__raw__)).to.not.contain('********');
     });
 
+    var prompt = stripAnsi(this.rl.output.__raw__)
     this.rl.emit('line', 'Inquirer');
+    return promise;
+  });
+
+  it('should mask the input with "*" if the `mask` option was provided by the user was `true`', function () {
+    this.fixture.mask = true
+    var password = new Password(this.fixture, this.rl);
+    var rl = this.rl;
+
+    var promise = password.run().then(function (answer) {
+      expect(answer).to.equal('Inquirer');
+      expect(stripAnsi(rl.output.__raw__)).to.contain('********');
+    });
+
+    var prompt = stripAnsi(this.rl.output.__raw__)
+    this.rl.emit('line', 'Inquirer');
+
+    return promise;
+  });
+
+  it('should mask the input if a `mask` string was provided by the user', function () {
+    this.fixture.mask = '#'
+    var password = new Password(this.fixture, this.rl);
+    var rl = this.rl;
+    var fixture = this.fixture;
+
+    var promise = password.run().then(function (answer) {
+      expect(answer).to.equal('Inquirer');
+      expect(stripAnsi(rl.output.__raw__)).to.contain('########');
+    });
+
+    var prompt = stripAnsi(this.rl.output.__raw__)
+    this.rl.emit('line', 'Inquirer');
+
+    return promise;
   });
 });
