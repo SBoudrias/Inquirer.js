5b3a4a2190337f9840e097337f5be53c881ffd02
Simon Boudrias <admin@simonboudrias.com>
2017-04-17 22:52:02 +0800

    Add masked password example

--- examples/password.js
+++ examples/password.js
@@ -8,8 +8,14 @@ var inquirer = require('..');
 inquirer.prompt([
   {
     type: 'password',
-    message: 'Enter your git password',
-    name: 'password'
+    message: 'Enter a password',
+    name: 'password1'
+  },
+  {
+    type: 'password',
+    message: 'Enter a masked password',
+    name: 'password2',
+    mask: '*'
   }
 ]).then(function (answers) {
   console.log(JSON.stringify(answers, null, '  '));
