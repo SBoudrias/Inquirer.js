b4fb37345b8adf0730b3d047b25a819f61baa458
Nicolas Gotchac <ngotchac@gmail.com>
2017-06-08 19:27:21 +0200

    Add example and fix hidden password validation

--- examples/password.js
+++ examples/password.js
@@ -8,6 +8,18 @@ var inquirer = require('..');
 inquirer.prompt([
   {
     type: 'password',
+    message: 'Enter "Passw0rd"',
+    name: 'password3',
+    validate: (input) => {
+      if (input !== 'Passw0rd') {
+        return 'invalid password'
+      }
+
+      return true;
+    }
+  },
+  {
+    type: 'password',
     message: 'Enter a password',
     name: 'password1'
   },
