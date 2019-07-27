e3cdc0c4a1c0e97ad8bf70f61abdcb1a14db3af4
Simon Boudrias <admin@simonboudrias.com>
2019-07-27 12:54:05 +0300

    Refactor core API with a react hook inspired API

---
+++ packages/core/lib/options.js
@@ -0,0 +1,12 @@
+exports.getPromptConfig = async options => {
+  let message = options.message;
+  if (typeof options.message === 'function') {
+    message = message();
+  }
+
+  return {
+    validate: () => true,
+    ...options,
+    message: await message
+  };
+};
--- packages/input/index.js
+++ packages/input/index.js
@@ -1,33 +1,57 @@
-const { createPrompt } = require('@inquirer/core');
+const { createPrompt, useState, useKeypress } = require('@inquirer/core/hooks');
+const { usePrefix } = require('@inquirer/core/lib/prefix');
 const chalk = require('chalk');
 
-module.exports = createPrompt(
-  {
-    canRemoveDefault: true,
-    onKeypress: (value, key, { canRemoveDefault }, setState) => {
-      const newState = { canRemoveDefault: !value };
+module.exports = createPrompt((config, done) => {
+  const [status, setStatus] = useState('pending');
+  const [errorMsg, setError] = useState();
+  const [value, setValue] = useState(config.default || '');
 
-      // Allow user to remove the default value by pressing backspace
-      if (canRemoveDefault && key.name === 'backspace') {
-        newState.default = undefined;
+  const isLoading = status === 'loading';
+  const prefix = usePrefix(isLoading);
+
+  useKeypress(async (key, rl) => {
+    // Ignore keypress while our prompt is doing other processing.
+    if (status !== 'pending') {
+      return;
+    }
+
+    if (key.name === 'enter' || key.name === 'return') {
+      setStatus('loading');
+      const isValid = await config.validate(value);
+      if (isValid === true) {
+        setStatus('done');
+        done(value);
+      } else {
+        // TODO: Can we keep the value after validation failure?
+        // `rl.line = value` works but it looses the cursor position.
+        setValue('');
+        setError(isValid || 'You must provide a valid value');
+        setStatus('pending');
       }
-
-      setState(newState);
+    } else {
+      setValue(rl.line);
+      setError(undefined);
     }
-  },
-  state => {
-    const { prefix, value = '', status } = state;
-    const message = chalk.bold(state.message);
-    let formattedValue = value;
-    if (status === 'done') {
-      formattedValue = chalk.cyan(value || state.default || '');
-    }
+  });
 
-    let defaultValue = '';
-    if (state.default && status !== 'done' && !value) {
-      defaultValue = chalk.dim(` (${state.default})`);
-    }
-
-    return `${prefix} ${message}${defaultValue} ${formattedValue}`;
+  const message = chalk.bold(config.message);
+  let formattedValue = value;
+  if (typeof config.transformer === 'function') {
+    formattedValue = config.transformer(value, { isFinal: status === 'done' });
+  } else if (status === 'done') {
+    formattedValue = chalk.cyan(value);
   }
-);
+
+  let defaultValue = '';
+  if (config.default && status !== 'done' && !value) {
+    defaultValue = chalk.dim(` (${config.default})`);
+  }
+
+  let error = '';
+  if (errorMsg) {
+    error = chalk.red(`> ${errorMsg}`);
+  }
+
+  return [`${prefix} ${message}${defaultValue} ${formattedValue}`, error];
+});
