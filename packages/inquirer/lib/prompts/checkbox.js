babae12f95d47a4c48ba3b0231c02258181f42dd
Quinlan Jung <quin@expo.io>
2020-06-03 17:00:28 -0700

    rebase @NTag fix: Bugfix for checkbox with multilines items

--- packages/inquirer/lib/prompts/checkbox.js
+++ packages/inquirer/lib/prompts/checkbox.js
@@ -6,7 +6,7 @@
 var _ = {
   isArray: require('lodash/isArray'),
   map: require('lodash/map'),
-  isString: require('lodash/isString')
+  isString: require('lodash/isString'),
 };
 var chalk = require('chalk');
 var cliCursor = require('cli-cursor');
@@ -25,7 +25,7 @@ class CheckboxPrompt extends Base {
     }
 
     if (_.isArray(this.opt.default)) {
-      this.opt.choices.forEach(function(choice) {
+      this.opt.choices.forEach(function (choice) {
         if (this.opt.default.indexOf(choice.value) >= 0) {
           choice.checked = true;
         }
@@ -109,8 +109,20 @@ class CheckboxPrompt extends Base {
       var indexPosition = this.opt.choices.indexOf(
         this.opt.choices.getChoice(this.pointer)
       );
+      var realIndexPosition =
+        this.opt.choices.reduce(function (acc, value, i) {
+          if (i > indexPosition) {
+            return acc;
+          }
+          if (value.type === 'separator') {
+            return acc + 1;
+          }
+          var l = value.name;
+          l = l.split('\n');
+          return acc + l.length;
+        }, 0) - 1;
       message +=
-        '\n' + this.paginator.paginate(choicesStr, indexPosition, this.opt.pageSize);
+        '\n' + this.paginator.paginate(choicesStr, realIndexPosition, this.opt.pageSize);
     }
 
     if (error) {
@@ -140,7 +152,7 @@ class CheckboxPrompt extends Base {
   }
 
   getCurrentValue() {
-    var choices = this.opt.choices.filter(function(choice) {
+    var choices = this.opt.choices.filter(function (choice) {
       return Boolean(choice.checked) && !choice.disabled;
     });
 
@@ -177,12 +189,12 @@ class CheckboxPrompt extends Base {
 
   onAllKey() {
     var shouldBeChecked = Boolean(
-      this.opt.choices.find(function(choice) {
+      this.opt.choices.find(function (choice) {
         return choice.type !== 'separator' && !choice.checked;
       })
     );
 
-    this.opt.choices.forEach(function(choice) {
+    this.opt.choices.forEach(function (choice) {
       if (choice.type !== 'separator') {
         choice.checked = shouldBeChecked;
       }
@@ -192,7 +204,7 @@ class CheckboxPrompt extends Base {
   }
 
   onInverseKey() {
-    this.opt.choices.forEach(function(choice) {
+    this.opt.choices.forEach(function (choice) {
       if (choice.type !== 'separator') {
         choice.checked = !choice.checked;
       }
@@ -219,7 +231,7 @@ function renderChoices(choices, pointer) {
   var output = '';
   var separatorOffset = 0;
 
-  choices.forEach(function(choice, i) {
+  choices.forEach(function (choice, i) {
     if (choice.type === 'separator') {
       separatorOffset++;
       output += ' ' + choice + '\n';
