73cbb1fe9e25859c8c9abedd6d1f3bcc39edeb8b
Simon Boudrias <admin@simonboudrias.com>
2023-04-29 16:53:31 -0400

    Bump dev dependencies

--- tools/rename-ext.ts
+++ tools/rename-ext.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import fs from 'node:fs';
-import { globby } from 'globby';
+import globby from 'globby';
 import * as url from 'node:url';
 
 // Force execution from the root of the repo. By default, the cwd will be each packages.
@@ -10,7 +10,7 @@ process.chdir(path.join(__dirname, '..'));
 // Because we're using .mts files, TS compiles to .mjs files disregarding the target. So here we
 // manually rename the common.js files to .js
 const paths = await globby(['packages/**/dist/cjs/**/*.mjs', '!**/node_modules']);
-paths.forEach(async (pathname) => {
+paths.forEach(async (pathname: string) => {
   const newPath = path.format({
     ...path.parse(pathname),
     base: '',
