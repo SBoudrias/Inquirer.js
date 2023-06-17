6dcbc01218efa27c6338219ee059bb0543849010
Simon Boudrias <admin@simonboudrias.com>
2023-06-17 14:42:35 -0400

    Update vitest & track all coverage data (incl. not yet tested packages)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig, defaultExclude } from 'vitest/config';
+import { defineConfig, defaultExclude, coverageConfigDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     exclude: ['integration/**', ...defaultExclude],
+    coverage: {
+      provider: 'v8',
+      all: true,
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'tools/**',
+        'packages/inquirer/examples/**',
+        'packages/demo/**',
+      ],
+    },
   },
 });
