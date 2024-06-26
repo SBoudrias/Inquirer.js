f8392e31fdfc2b0459a50041d69ac55202bbdf53
Simon Boudrias <admin@simonboudrias.com>
2024-06-26 12:25:45 -0400

    Chore(inquirer): Remove lodash clone/defaults dependencies

--- packages/inquirer/lib/prompts/base.js
+++ packages/inquirer/lib/prompts/base.js
@@ -1,14 +1,7 @@
-import defaults from 'lodash.defaults';
-import clone from 'lodash.clone';
 /**
  * Base prompt implementation
  * Should be extended by prompt types.
  */
-const _ = {
-  defaults,
-  clone,
-};
-
 import chalk from 'chalk';
 import runAsync from 'run-async';
 import { filter, flatMap, share, take, takeUntil } from 'rxjs';
@@ -24,7 +17,7 @@ export default class Prompt {
     });
 
     // Set defaults prompt options
-    this.opt = _.defaults(_.clone(question), {
+    this.opt = {
       validate: () => true,
       validatingText: '',
       filter: (val) => val,
@@ -33,7 +26,8 @@ export default class Prompt {
       suffix: '',
       prefix: chalk.green('?'),
       transformer: (val) => val,
-    });
+      ...question,
+    };
 
     // Make sure name is present
     if (!this.opt.name) {
