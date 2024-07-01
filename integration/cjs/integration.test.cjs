6c764ac584a798f006cb809b0f489dbad1706198
Simon Boudrias <admin@simonboudrias.com>
2024-07-01 14:13:11 -0400

    Chore(integration): Add more integration tests. Covers inquirer, core & single prompt modules

--- integration/cjs/integration.test.cjs
+++ integration/cjs/integration.test.cjs
@@ -2,7 +2,17 @@
 const test = require('node:test');
 const assert = require('node:assert');
 const { input } = require('@inquirer/prompts');
+const { createPrompt } = require('@inquirer/core');
+const defaultInput = require('@inquirer/input').default;
 
-test('should export modules', () => {
+test('[CJS] @inquirer/prompts should be exported', () => {
   assert(input instanceof Function);
+});
+
+test('[CJS] @inquirer/input should be exported', () => {
+  assert(defaultInput instanceof Function);
+});
+
+test('[CJS] @inquirer/core should export createPrompt', () => {
+  assert(createPrompt instanceof Function);
 });
--- integration/esm/integration.test.mjs
+++ integration/esm/integration.test.mjs
@@ -1,8 +1,25 @@
 /* eslint-disable n/no-unsupported-features/node-builtins */
 import { input } from '@inquirer/prompts';
+import defaultInput from '@inquirer/input';
+import { createPrompt } from '@inquirer/core';
+import inquirer, { createPromptModule } from 'inquirer';
 import test from 'node:test';
 import assert from 'node:assert';
 
-test('should export modules', () => {
+test('[ESM] @inquirer/prompts should be exported', () => {
   assert(input instanceof Function);
+});
+
+test('[ESM] @inquirer/input should be exported', () => {
+  assert(defaultInput instanceof Function);
+});
+
+test('[ESM] @inquirer/core should export createPrompt', () => {
+  assert(createPrompt instanceof Function);
+});
+
+test('[ESM] inquirer should be exported', () => {
+  assert(inquirer.prompt instanceof Function);
+  assert(inquirer.createPromptModule instanceof Function);
+  assert(createPromptModule instanceof Function);
 });
