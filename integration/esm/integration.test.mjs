fb32a002cab46fc6ac8f219a66735a4a520a5bb3
Simon Boudrias <admin@simonboudrias.com>
2024-07-03 17:30:32 -0400

    Fix(inquirer): Fix changed paths from internals used by plugin vendors. (#1457)
    * Chore(turbo): Consider root tsconfig.json as a dependency
    
    * Fix(inquirer): Fix changed paths from internals used by plugin vendors. Fix #1456

--- integration/esm/integration.test.mjs
+++ integration/esm/integration.test.mjs
@@ -6,6 +6,13 @@ import inquirer, { createPromptModule } from 'inquirer';
 import test from 'node:test';
 import assert from 'node:assert';
 
+import Base from 'inquirer/lib/prompts/base.js';
+import Choices from 'inquirer/lib/objects/choices.js';
+import Separator from 'inquirer/lib/objects/separator.js';
+import observe from 'inquirer/lib/utils/events.js';
+import * as utils from 'inquirer/lib/utils/readline.js';
+import Paginator from 'inquirer/lib/utils/paginator.js';
+
 test('[ESM] @inquirer/prompts should be exported', () => {
   assert(input instanceof Function);
 });
@@ -22,4 +29,13 @@ test('[ESM] inquirer should be exported', () => {
   assert(inquirer.prompt instanceof Function);
   assert(inquirer.createPromptModule instanceof Function);
   assert(createPromptModule instanceof Function);
+});
+
+test('[ESM] inquirer custom prompts util paths are stable', () => {
+  assert(Base != null);
+  assert(Choices != null);
+  assert(Separator != null);
+  assert(observe != null);
+  assert(utils != null);
+  assert(Paginator != null);
 });
