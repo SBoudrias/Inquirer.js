cd168c2264be8e7dabaf75a0bc33abfd68458e63
Simon Boudrias <admin@simonboudrias.com>
2024-07-07 13:46:53 -0400

    Test: Unify integration tests styles with other tests

--- integration/esm/integration.test.mjs
+++ integration/esm/integration.test.mjs
@@ -3,7 +3,7 @@ import { input } from '@inquirer/prompts';
 import defaultInput from '@inquirer/input';
 import { createPrompt } from '@inquirer/core';
 import inquirer, { createPromptModule } from 'inquirer';
-import test from 'node:test';
+import { describe, it } from 'node:test';
 import assert from 'node:assert';
 
 import Base from 'inquirer/lib/prompts/base.js';
@@ -13,29 +13,31 @@ import observe from 'inquirer/lib/utils/events.js';
 import * as utils from 'inquirer/lib/utils/readline.js';
 import Paginator from 'inquirer/lib/utils/paginator.js';
 
-test('[ESM] @inquirer/prompts should be exported', () => {
-  assert(input instanceof Function);
-});
+describe('ESM Integration', () => {
+  it('@inquirer/prompts should be exported', () => {
+    assert(input instanceof Function);
+  });
 
-test('[ESM] @inquirer/input should be exported', () => {
-  assert(defaultInput instanceof Function);
-});
+  it('@inquirer/input should be exported', () => {
+    assert(defaultInput instanceof Function);
+  });
 
-test('[ESM] @inquirer/core should export createPrompt', () => {
-  assert(createPrompt instanceof Function);
-});
+  it('@inquirer/core should export createPrompt', () => {
+    assert(createPrompt instanceof Function);
+  });
 
-test('[ESM] inquirer should be exported', () => {
-  assert(inquirer.prompt instanceof Function);
-  assert(inquirer.createPromptModule instanceof Function);
-  assert(createPromptModule instanceof Function);
-});
+  it('inquirer should be exported', () => {
+    assert(inquirer.prompt instanceof Function);
+    assert(inquirer.createPromptModule instanceof Function);
+    assert(createPromptModule instanceof Function);
+  });
 
-test('[ESM] inquirer custom prompts util paths are stable', () => {
-  assert(Base != null);
-  assert(Choices != null);
-  assert(Separator != null);
-  assert(observe != null);
-  assert(utils != null);
-  assert(Paginator != null);
+  it('inquirer custom prompts util paths are stable', () => {
+    assert(Base != null);
+    assert(Choices != null);
+    assert(Separator != null);
+    assert(observe != null);
+    assert(utils != null);
+    assert(Paginator != null);
+  });
 });
