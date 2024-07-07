f7e61861b81e4bd438cc7f117ad5427ad9506a32
Simon Boudrias <admin@simonboudrias.com>
2024-07-07 13:46:53 -0400

    Test: Unify integration tests styles with other tests

--- integration/cjs/integration.test.cjs
+++ integration/cjs/integration.test.cjs
@@ -1,18 +1,20 @@
 /* eslint-disable n/no-unsupported-features/node-builtins */
-const test = require('node:test');
+const { describe, it } = require('node:test');
 const assert = require('node:assert');
 const { input } = require('@inquirer/prompts');
 const { createPrompt } = require('@inquirer/core');
 const defaultInput = require('@inquirer/input').default;
 
-test('[CJS] @inquirer/prompts should be exported', () => {
-  assert(input instanceof Function);
-});
+describe('CommonJS Integration', () => {
+  it('@inquirer/prompts should be exported', () => {
+    assert(input instanceof Function);
+  });
 
-test('[CJS] @inquirer/input should be exported', () => {
-  assert(defaultInput instanceof Function);
-});
+  it('@inquirer/input should be exported', () => {
+    assert(defaultInput instanceof Function);
+  });
 
-test('[CJS] @inquirer/core should export createPrompt', () => {
-  assert(createPrompt instanceof Function);
+  it('@inquirer/core should export createPrompt', () => {
+    assert(createPrompt instanceof Function);
+  });
 });
