bfcb500c52cf08831c25a2990585853fd30d790b
Simon Boudrias <admin@simonboudrias.com>
2025-05-25 16:23:57 -0400

    Refactor: usePointerPosition()

--- packages/core/src/lib/pagination/use-pagination.ts
+++ packages/core/src/lib/pagination/use-pagination.ts
@@ -1,94 +1,91 @@
 import type { Prettify } from '@inquirer/type';
 import { useRef } from '../use-ref.ts';
-import { readlineWidth } from '../utils.ts';
-import { type Theme } from '../theme.ts';
-import { breakLines } from '../utils.ts';
+import { readlineWidth, breakLines } from '../utils.ts';
 
-/**
- * Creates the next position for the pointer considering a finite list of
- * items to be rendered on a page.
- *
- * The goal is to keep the pointer in the middle of the page whenever possible, until
- * we reach the bounds of the list (top or bottom).
- */
-function finite({
+function usePointerPosition({
   active,
-  pageSize,
   renderedItems,
+  pageSize,
+  loop,
 }: {
   active: number;
-  pageSize: number;
   renderedItems: string[][];
-}): number {
+  pageSize: number;
+  loop: boolean;
+}) {
+  const state = useRef<{ lastPointer: number; lastActive: number | undefined }>({
+    lastPointer: active,
+    lastActive: undefined,
+  });
+  const { lastPointer, lastActive } = state.current;
+
   const middle = Math.floor(pageSize / 2);
   const renderedLength = renderedItems.reduce((acc, item) => acc + item.length, 0);
   const defaultPointerPosition = renderedItems
     .slice(0, active)
     .reduce((acc, item) => acc + item.length, 0);
 
-  // If the whole rendered list fit within a single page, place the pointer where the item is rendered.
-  if (renderedLength <= pageSize) {
-    return defaultPointerPosition;
+  let pointer = defaultPointerPosition;
+
+  if (renderedLength > pageSize) {
+    if (loop) {
+      /**
+       * Creates the next position for the pointer considering an infinitely
+       * looping list of items to be rendered on the page.
+       *
+       * The goal is to progressively move the cursor to the middle position as the user move down, and then keep
+       * the cursor there. When the user move up, maintain the cursor position.
+       */
+
+      // By default, keep the cursor position as-is.
+      pointer = lastPointer;
+
+      if (
+        // First render, skip this logic.
+        lastActive != null &&
+        // Only move the pointer down when the user moves down.
+        lastActive < active &&
+        // Check user didn't move up across page boundary.
+        active - lastActive < pageSize
+      ) {
+        pointer = Math.min(
+          // Furthest allowed position for the pointer is the middle of the list
+          middle,
+          Math.abs(active - lastActive) === 1
+            ? // If the user moved by one item, move the pointer to the natural position of the active item as
+              // long as it doesn't move the cursor up.
+              Math.max(defaultPointerPosition, lastPointer)
+            : // Otherwise, move the pointer down by the difference between the active and last active item.
+              lastPointer + active - lastActive,
+        );
+      }
+    } else {
+      /**
+       * Creates the next position for the pointer considering a finite list of
+       * items to be rendered on a page.
+       *
+       * The goal is to keep the pointer in the middle of the page whenever possible, until
+       * we reach the bounds of the list (top or bottom). In which case, the cursor moves progressively
+       * to the bottom or top of the list.
+       */
+      const spaceUnderActive = renderedItems
+        .slice(active)
+        .reduce((acc, item) => acc + item.length, 0);
+
+      pointer =
+        spaceUnderActive < pageSize - middle
+          ? // If the active item is near the end of the list, progressively move the cursor towards the end.
+            pageSize - spaceUnderActive
+          : // Otherwise, progressively move the pointer to the middle of the list.
+            Math.min(defaultPointerPosition, middle);
+    }
   }
 
-  // If the active item is near the end of the list, progressively move the cursor towards the end.
-  const spaceUnderActive = renderedItems
-    .slice(active)
-    .reduce((acc, item) => acc + item.length, 0);
-  if (spaceUnderActive < pageSize - middle) {
-    return pageSize - spaceUnderActive;
-  }
+  // Save state for the next render
+  state.current.lastPointer = pointer;
+  state.current.lastActive = active;
 
-  // Otherwise, progressively move the pointer to the middle of the list.
-  return Math.min(defaultPointerPosition, middle);
-}
-
-/**
- * Creates the next position for the pointer considering an infinitely
- * looping list of items to be rendered on the page.
- */
-function infinite({
-  active,
-  lastActive,
-  renderedItems,
-  pageSize,
-  lastPointer,
-}: {
-  active: number;
-  lastActive: number | undefined;
-  renderedItems: string[][];
-  pageSize: number;
-  lastPointer: number;
-}): number {
-  const middle = Math.floor(pageSize / 2);
-  const renderedLength = renderedItems.reduce((acc, item) => acc + item.length, 0);
-  const defaultPointerPosition = renderedItems
-    .slice(0, active)
-    .reduce((acc, item) => acc + item.length, 0);
-
-  // If the whole rendered list fit within a single page, place the pointer where the item is rendered.
-  if (renderedLength <= pageSize) return defaultPointerPosition;
-
-  if (
-    // If we don't have a previous selection, skip this logic.
-    lastActive != null &&
-    // Only progressively move the pointer down if the user is moving down.
-    lastActive < active &&
-    active - lastActive < pageSize
-  ) {
-    return Math.min(
-      // Furthest allowed position for the pointer is the middle of the list
-      middle,
-      Math.abs(active - lastActive) === 1
-        ? // If the user moved by one item, move the pointer to the natural position of the active item as
-          // long as it doesn't move the cursor up.
-          Math.max(defaultPointerPosition, lastPointer)
-        : // Otherwise, move the pointer down by the difference between the active and last active item.
-          lastPointer + active - lastActive,
-    );
-  }
-
-  return lastPointer;
+  return pointer;
 }
 
 export function usePagination<T>({
@@ -113,14 +110,8 @@ export function usePagination<T>({
   pageSize: number;
   /** Allows creating an infinitely looping list. `true` if unspecified. */
   loop?: boolean;
-  theme?: Theme;
 }): string {
   const width = readlineWidth();
-  const state = useRef<{ lastPointer: number; lastActive: number | undefined }>({
-    lastPointer: active,
-    lastActive: undefined,
-  });
-
   const bound = (num: number) => ((num % items.length) + items.length) % items.length;
 
   const renderedItems: string[][] = items.map((item, index) => {
@@ -133,19 +124,7 @@ export function usePagination<T>({
   const renderedLength = renderedItems.reduce((acc, item) => acc + item.length, 0);
   const renderItemAtIndex = (index: number): string[] => renderedItems[index] ?? [];
 
-  const pointer = loop
-    ? infinite({
-        active,
-        lastActive: state.current.lastActive,
-        lastPointer: state.current.lastPointer,
-        renderedItems,
-        pageSize,
-      })
-    : finite({
-        active,
-        renderedItems,
-        pageSize,
-      });
+  const pointer = usePointerPosition({ active, renderedItems, pageSize, loop });
 
   // Render the active item to decide the position.
   // If the active item fits under the pointer, we render it there.
@@ -200,10 +179,6 @@ export function usePagination<T>({
     bufferPointer -= linesToAdd.length;
     itemPointer = bound(itemPointer - 1);
   }
-
-  // Save state for the next render
-  state.current.lastPointer = pointer;
-  state.current.lastActive = active;
 
   return pageBuffer.filter((line) => typeof line === 'string').join('\n');
 }
